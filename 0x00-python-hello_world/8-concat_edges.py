915a30b33a44cdb0384d9b07fe4f3387b5c7f6a1
wrightkhlebisol <calebogundiya@gmail.com>
2023-08-07 22:44:00 +0100

    fix line count

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-new_str = str[39:67] + str[107:112] + str[0:7]
-str = new_str
+str = str[39:67] + str[107:112] + str[0:7]
 print(str)
