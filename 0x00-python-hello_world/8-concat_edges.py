2b10e981e22d5f0b271c725d0fc7ac672206ea7b
wrightkhlebisol <calebogundiya@gmail.com>
2023-08-07 22:42:00 +0100

    concat edges

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+new_str = str[39:67] + str[107:112] + str[0:7]
+str = new_str
+print(str)
