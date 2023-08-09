d2a12198302a50e4b2164c4767e2cf87d40bc82d
wrightkhlebisol <calebogundiya@gmail.com>
2023-08-09 03:19:24 +0100

    fix indent error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,11 +2,11 @@
 import random
 number = random.randint(-10000, 10000)
 
-new_num = str(number)[-1]
-new_num = int(new_num)
-if new_num  > 5:
-	print(f"Last digit of {number:d} is greater than 5")
+new_num = int(str(number)[-1])
+
+if new_num > 5:
+    print(f"Last digit of {number:d} is greater than 5")
 if new_num < 6 and new_num != 0:
-	print(f"Last digit of {number:d} is less than 6 and not 0")
+    print(f"Last digit of {number:d} is less than 6 and not 0")
 if new_num == 0:
-	print(f"Last digit of {number:d} is 0")
+    print(f"Last digit of {number:d} is 0")
