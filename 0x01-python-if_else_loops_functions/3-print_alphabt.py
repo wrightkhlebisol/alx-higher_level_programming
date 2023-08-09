19f6e81112336fa299c2312c0d7230ab9a223779
wrightkhlebisol <calebogundiya@gmail.com>
2023-08-09 04:09:23 +0100

    alpha no e and p

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,1 +1,7 @@
-#/usr/bin/python3
+#!/usr/bin/python3
+asc_range = list(range(97, 123))
+
+for n in asc_range:
+    if n == 101 or n == 113:
+        continue
+    print("{}".format(chr(n)), end="")
