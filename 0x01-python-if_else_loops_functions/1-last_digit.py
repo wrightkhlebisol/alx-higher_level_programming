981d5931bc06be8213c694424ff3a68811b8109a
wrightkhlebisol <calebogundiya@gmail.com>
2023-08-10 19:06:15 +0100

    fix negative

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,10 @@
 import random
 number = random.randint(-10000, 10000)
 
-new_num = int(str(number)[-1])
+if number < 0:
+    new_num = int(str(number)[-1]) * -1
+else:
+    new_num = int(str(number)[-1])
 
 if new_num > 5:
     print(f"Last digit of {number:d} \
