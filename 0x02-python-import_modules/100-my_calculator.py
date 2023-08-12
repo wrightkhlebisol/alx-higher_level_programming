f60876ddd38e7c11c048acf32ed22afc7d945a8e
wrightkhlebisol <calebogundiya@gmail.com>
2023-08-12 15:02:42 +0100

    fix exit code

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,15 +3,14 @@ if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
     if len(sys.argv) < 4:
-        sys.stderr.write("Usage: ./100-my_calculator.py <a> <operator> <b>\n")
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     else:
         a = int(sys.argv[1])
         b = int(sys.argv[3])
         sign, sign_list = sys.argv[2], ["+", "-", "*", "/"]
         if sign not in sign_list:
-            sys.stderr.write("Unknown operator. Available \
-operators: +, -, * and /\n")
+            print("Unknown operator. Available operators: +, -, * and /")
             sys.exit(1)
         else:
             if sign == '+':
