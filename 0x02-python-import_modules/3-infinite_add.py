0cd2ecec23867829e9cb484f5c64ee05a89d7970
wrightkhlebisol <calebogundiya@gmail.com>
2023-08-12 10:38:42 +0100

    infinite add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,9 @@
-#/usr/bin/python3
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    tot_sum = 0
+    if len(sys.argv) > 1:
+        for n in range(1, len(sys.argv)):
+            tot_sum += int(sys.argv[n])
+    print("{}".format(tot_sum))
