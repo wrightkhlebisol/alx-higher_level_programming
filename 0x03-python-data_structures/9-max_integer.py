59a9c1cc1c11e419744f0effb0315617507928d4
wrightkhlebisol <calebogundiya@gmail.com>
2023-08-14 20:03:03 +0100

    max int

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,1 +1,9 @@
-#/usr/bin/python3
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list == None:
+        return None
+    max_int = 0
+    for n in my_list:
+        if n > max_int:
+            max_int = n
+    return max_int
