7c1ed6b2c26726c9b218dfeb63092c40f384602e
wrightkhlebisol <calebogundiya@gmail.com>
2023-08-15 20:11:49 +0100

    fix list is none

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list == None:
+    if my_list == []:
         return None
-    max_int = 0
+
+    max_int = my_list[0]
     for n in my_list:
         if n > max_int:
             max_int = n
