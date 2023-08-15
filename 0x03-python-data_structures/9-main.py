0ac4b41b57a5cb42261c721a7ed60892a60cd386
wrightkhlebisol <calebogundiya@gmail.com>
2023-08-15 23:31:52 +0100

    push all

--- 0x03-python-data_structures/9-main.py
+++ 0x03-python-data_structures/9-main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 max_integer = __import__('9-max_integer').max_integer
 
-my_list = []
 #my_list = [1, 90, 2, 13, 34, 5, -13, 3]
+my_list = [-2, -5, -3, -1]
 max_value = max_integer(my_list)
 print("Max: {}".format(max_value))
 
