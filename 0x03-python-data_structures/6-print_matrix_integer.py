15a8be15c294f1f6b83df5d4d582286fc84ec213
wrightkhlebisol <calebogundiya@gmail.com>
2023-08-15 22:37:10 +0100

    print matrix integer

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     if matrix is None:
         print("{}".format(matrix))
     elif len(matrix) <= 1:
-        print()
+        print("")
     else:
         for row in matrix:
             print("{} {} {}".format(row[0], row[1], row[2]))
