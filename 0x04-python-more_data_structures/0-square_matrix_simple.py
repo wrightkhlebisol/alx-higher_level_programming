1db339165bbe32390672af2a85f78525c9685f84
wrightkhlebisol <calebogundiya@gmail.com>
2023-08-17 00:16:08 +0100

    square matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,1 +1,7 @@
-#/usr/bin/python3
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_mat = []
+    if len(matrix) > 0:
+        for row in matrix:
+            new_mat.append([x**2 for x in row])
+    return new_mat
