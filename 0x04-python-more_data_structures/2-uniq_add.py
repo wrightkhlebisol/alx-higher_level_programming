345da477e3082c3ed584b69825471187ada7f4f2
wrightkhlebisol <calebogundiya@gmail.com>
2023-08-17 01:48:16 +0100

    unique add

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,1 +1,8 @@
-#/usr/bin/python3
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    _sum = 0
+    if len(my_list) > 0:
+        for val in set(my_list):
+            _sum += val
+    return _sum
+
