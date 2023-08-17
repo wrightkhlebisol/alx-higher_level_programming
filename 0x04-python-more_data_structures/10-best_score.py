2d2f57d53336f35a4db69e1f4a8f0fe6d5b7adf3
wrightkhlebisol <calebogundiya@gmail.com>
2023-08-17 02:56:15 +0100

    max score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,1 +1,11 @@
-#/usr/bin/python3
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    max_key = None
+    max_val = 0
+    if a_dictionary is None:
+        return None
+    for key in list(a_dictionary):
+        if a_dictionary[key] > max_val:
+            max_val = a_dictionary[key]
+            max_key = key
+    return max_key
