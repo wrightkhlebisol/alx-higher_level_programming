9c3688b6b9ac189c0e096ae6e54401ab516b7a26
wrightkhlebisol <calebogundiya@gmail.com>
2023-08-28 22:34:08 +0100

    safe print list int and not strings

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,13 @@
-#/usr/bin/python3
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    num_printed = 0
+    try:
+        for i in range(x):
+            if type(my_list[i]) == int:
+                print(my_list[i], end='')
+                num_printed += 1
+        print()
+        return num_printed
+    except (TypeError, ValueError):
+        print()
+        return num_printed
