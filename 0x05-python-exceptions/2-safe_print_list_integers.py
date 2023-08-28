c091ce888fef7e931a364d0be910821dd7a9d252
wrightkhlebisol <calebogundiya@gmail.com>
2023-08-29 00:52:30 +0100

    fix requirement fail

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     try:
         for i in range(x):
             if type(my_list[i]) == int:
-                print(my_list[i], end='')
+                print("{:d}".format(my_list[i]), end='')
                 num_printed += 1
         print()
         return num_printed
