414173faeb936a40883288b56efb2d5baf981608
wrightkhlebisol <calebogundiya@gmail.com>
2023-08-29 01:46:35 +0100

    safe function

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,6 +6,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except (ValueError, TypeError) as err:
+    except Exception as err:
         sys.stderr.write("Exception: {}".format(err))
         return False
--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,1 +1,11 @@
-#/usr/bin/python3
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        return fct(*args)
+        print()
+    except Exception as err:
+        sys.stderr.write("Exception: {}".format(err))
+        return None
