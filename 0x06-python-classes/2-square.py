05d3e3e9f20f00e62449b85d9db02b36e2201683
wrightkhlebisol <calebogundiya@gmail.com>
2023-08-31 01:38:25 +0100

    validate square input

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,16 @@
-#/usr/bin/python3
+#!/usr/bin/python3
+"""2-square.py"""
+
+
+class Square:
+    """Size validating class."""
+
+    def __init__(self, size=0):
+        """Initializes the size variable"""
+
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
