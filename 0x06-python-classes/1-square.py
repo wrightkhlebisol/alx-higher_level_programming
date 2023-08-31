a9f285ea9ba919c38fed9cf91f0957673ad96313
wrightkhlebisol <calebogundiya@gmail.com>
2023-08-31 01:12:54 +0100

    private property

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,9 @@
-#/usr/bin/python3
+#!/usr/bin/python3
+"""1-square.py"""
+
+
+class Square:
+    """Square with size"""
+    def __init__(self, size):
+        """ Initializes the instance data attributes """
+        self.__size = size
