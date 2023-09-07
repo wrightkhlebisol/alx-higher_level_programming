c8a79e149643bf922369bb47e3cd66b6eb1193de
wrightkhlebisol <calebogundiya@gmail.com>
2023-09-07 03:51:11 +0100

    fix any symbol and type

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -64,8 +64,8 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return f''
         for i in range(self.__height - 1):
-            print("{}".format(str(Rectangle.print_symbol)) * self.__width)
-        return f"{str(Rectangle.print_symbol)}" * self.__width
+            print(str(self.print_symbol) * self.__width)
+        return f"{str(self.print_symbol) * self.__width}"
 
     def __repr__(self):
         """String representation of rectangle"""
