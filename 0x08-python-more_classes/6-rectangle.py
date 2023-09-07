21fb3044ff665083a1222093eb574454a59178cb
wrightkhlebisol <calebogundiya@gmail.com>
2023-09-07 02:25:00 +0100

    class variable

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-"""4-rectangle.py"""
+"""6-rectangle.py"""
 
 
 class Rectangle:
     """Rectangle class"""
+    
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """Initialization method"""
         if type(width) is not int:
@@ -16,6 +19,7 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         self.__width = width
         self.__height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -66,3 +70,7 @@ class Rectangle:
     def __repr__(self):
         """String representation of rectangle"""
         return f"Rectangle({self.__width}, {self.__height})"
+
+    def __del__(self):
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
