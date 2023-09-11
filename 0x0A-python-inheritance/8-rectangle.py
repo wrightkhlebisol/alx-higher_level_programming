894b87f4db12bb906242afc9aa2b0b361b4a2ae5
wrightkhlebisol <calebogundiya@gmail.com>
2023-09-11 21:54:54 +0100

    inherit and validate

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,1 +1,29 @@
-#/usr/bin/python3
+#!/usr/bin/python3
+"""8-rectangle.py"""
+
+
+class BaseGeometry:
+    """Base Geometry class"""
+
+    def area(self):
+        """Unimplemented area"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Integer validator"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """Inherit BaseGeometry"""
+
+    def __init__(self, width, height):
+        """Initialization method"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
