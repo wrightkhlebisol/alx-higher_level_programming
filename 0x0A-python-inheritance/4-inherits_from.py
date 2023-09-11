da288673c063d70a199643399a6c206f79f97848
wrightkhlebisol <calebogundiya@gmail.com>
2023-09-11 23:38:08 +0100

    instance of a class inherited

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,1 +1,7 @@
-#/usr/bin/python3
+#!/usr/bin/python3
+"""4-inherits_from.py"""
+
+
+def inherits_from(obj, a_class):
+    """Inherits (directly or indirectly) from the specified class"""
+    return isinstance(obj, a_class)
