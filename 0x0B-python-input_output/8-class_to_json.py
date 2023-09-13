01f2efb8e15d9284ce45d874f6f7dc998f4ec4d8
wrightkhlebisol <calebogundiya@gmail.com>
2023-09-13 03:16:17 +0100

    dump and object

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+"""8-class_to_json.py"""
+import json
+
+
+def class_to_json(obj):
+    """dictionary description with simple data structure"""
+    return json.dumps(obj.__dict__)
