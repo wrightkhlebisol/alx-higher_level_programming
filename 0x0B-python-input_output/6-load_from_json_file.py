c2495a438e9ed9469fbcd94ebb433add2276247c
wrightkhlebisol <calebogundiya@gmail.com>
2023-09-13 01:43:43 +0100

    load from json file

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""6-load_from_json_file.py"""
+import json
+
+
+def load_from_json_file(filename):
+    """Load JSON from file"""
+    with open(filename) as f:
+        return json.load(f)
