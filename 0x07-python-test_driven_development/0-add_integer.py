6765ee030ee95997e0ecdbb33d4a65afe41f35bb
wrightkhlebisol <calebogundiya@gmail.com>
2023-09-13 20:17:43 +0100

    appropriate test and handle int

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,7 +4,4 @@
 
 def add_integer(a, b=98):
     """Add two integers with one named argument"""
-    try:
-        return int(a) + int(b)
-    except SyntaxError as e:
-        print(e, "Syntax error")
+    return int(a) + int(b)
