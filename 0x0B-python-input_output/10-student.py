5c648b7f87a8cf350225ff97cffd4c895bafd3b7
wrightkhlebisol <calebogundiya@gmail.com>
2023-09-13 03:52:54 +0100

    attrs

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""10-student.py"""
+
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        self_dict = self.__dict__
+        if attrs is None:
+            return self_dict
+        else:
+            new_dict = {}
+            for attr in attrs:
+                if self_dict.get(attr) is not None:
+                    new_dict[attr] = self_dict.get(attr)
+            return new_dict
