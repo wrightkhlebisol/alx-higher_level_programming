1e12bdbc353497dadd09f7ac803221b0b212346a
wrightkhlebisol <calebogundiya@gmail.com>
2023-09-17 23:38:16 +0100

    setup, rectangle, base

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""BASE CLASS: models/base.py"""
+
+
+class Base():
+    """Base class"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Class init method"""
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
