fdfcc4ad83fff83d529c652e5955512cae3bd6fa
wrightkhlebisol <calebogundiya@gmail.com>
2023-09-18 20:40:31 +0100

    save to files

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,3 +20,16 @@ class Base():
             return []
         else:
             return str(list_dictionaries)
+
+    @classmethod 
+    def save_to_file(cls, list_objs):
+        """Save json to file"""
+        file_name = list_objs[0].__class__.__name__ + ".json"
+        with open(file_name, 'w') as file:
+            if list_objs is None:
+                file.write("")
+            else:
+                new_list = list()
+                for list_obj in list_objs:
+                    new_list.append(list_obj.to_dictionary())
+                file.write(cls.to_json_string(new_list))
