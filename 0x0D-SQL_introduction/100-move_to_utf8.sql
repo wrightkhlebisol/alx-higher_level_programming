2e7fd94d2767e8d7c6d3f2b5d71b410635496c8d
wrightkhlebisol <calebogundiya@gmail.com>
2023-10-21 22:49:34 +0100

    reorder alter statement

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,5 @@
 -- Converts database to UTF8mb4_unicode_ci
-ALTER DATABASE `hbtn_0c_0` COLLATE 'utf8mb4_unicode_ci';
 USE `hbtn_0c_0`;
-ALTER TABLE `first_table` COLLATE 'utf8mb4_unicode_ci';
 ALTER TABLE `first_table` MODIFY `name` VARCHAR(256) COLLATE 'utf8mb4_unicode_ci';
+ALTER TABLE `first_table` COLLATE 'utf8mb4_unicode_ci';
+ALTER DATABASE `hbtn_0c_0` COLLATE 'utf8mb4_unicode_ci';
