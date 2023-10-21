8d4a8568d80465e70d17d1fdbd6aba5deb05ea1e
wrightkhlebisol <calebogundiya@gmail.com>
2023-10-21 22:24:18 +0100

    top city

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,6 @@
+-- Display top 3 of cities temperature during July and August ordered by temperature (desc)
+SELECT city, AVG(value) AS 'avg_temp' FROM `temperatures` 
+	WHERE month >= 7 and month <= 8
+	GROUP BY city
+	ORDER BY avg_temp DESC
+	LIMIT 3;
