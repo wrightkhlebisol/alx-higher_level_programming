6652de9917d6276cd65bfa7cc5842324d208e15b
wrightkhlebisol <calebogundiya@gmail.com>
2023-10-21 21:38:59 +0100

    alter charset and collation

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,4 @@
+-- Converts database to UTF8mb4_unicode_ci
+ALTER DATABASE `hbtn_0c_0` COLLATE 'utf8mb4_unicode_ci';
+ALTER TABLE `first_table` COLLATE 'utf8mb4_unicode_ci';
+ALTER TABLE `first_table` MODIFY `name` VARCHAR(256) COLLATE 'utf8mb4_unicode_ci';
