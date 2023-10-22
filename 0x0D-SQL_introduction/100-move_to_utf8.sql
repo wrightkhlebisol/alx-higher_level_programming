a569e483024109c45eaa337a39baf7ba56b727a7
wrightkhlebisol <calebogundiya@gmail.com>
2023-10-22 03:32:46 -0700

    use db

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,7 +1,8 @@
 -- Converts database to UTF8mb4_unicode_ci
+ALTER DATABASE `hbtn_0c_0` COLLATE 'utf8mb4_unicode_ci';
+USE `hbtn_0c_0`;
+ALTER TABLE `first_table` COLLATE 'utf8mb4_unicode_ci';
 USE `hbtn_0c_0`;
 ALTER TABLE `first_table` 
 	MODIFY `name` 
 	VARCHAR(256);
-ALTER TABLE `first_table` COLLATE 'utf8mb4_unicode_ci';
-ALTER DATABASE `hbtn_0c_0` COLLATE 'utf8mb4_unicode_ci';
