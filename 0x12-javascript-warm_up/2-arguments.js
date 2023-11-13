86d64e32cd603b437d59f91f7e70066cbbdbd2ad
wrightkhlebisol <calebogundiya@gmail.com>
2023-11-13 23:16:55 +0000

    check args

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const argLen = process.argv.length;
+if (argLen <= 2) {
+  console.log('No argument');
+} else if (argLen === 3) {
+  console.log('Argument Found');
+} else {
+  console.log('Arguments Found');
+}
