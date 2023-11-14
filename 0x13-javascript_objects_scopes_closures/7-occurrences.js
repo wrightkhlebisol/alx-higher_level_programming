106337328397daf765c073ec2b647675f4e3770f
wrightkhlebisol <calebogundiya@gmail.com>
2023-11-14 17:54:49 +0000

    get num of occurences

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+
+function nbOccurences (list, searchElement) {
+  let numOccur = 0;
+  let i = 0;
+
+  while (i < list.length) {
+    if (list[i] === searchElement) {
+      numOccur++;
+    }
+    i++;
+  }
+  return numOccur;
+}
+
+module.exports = { nbOccurences };
