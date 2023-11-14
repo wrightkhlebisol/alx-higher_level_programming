47e222cdd1bc56794ea497b22306707df8680b55
wrightkhlebisol <calebogundiya@gmail.com>
2023-11-14 00:05:34 +0000

    loop print

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+const numP = parseInt(process.argv[2]);
+
+if (!numP) {
+  console.log('Missing number of occurrences');
+} else {
+  let i = 0;
+  while (i < numP) {
+    console.log('C is fun');
+    i++;
+  }
+}
