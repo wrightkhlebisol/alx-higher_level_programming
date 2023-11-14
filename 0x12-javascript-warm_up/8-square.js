94014cea625ce83c3443c14c5065870fad515494
wrightkhlebisol <calebogundiya@gmail.com>
2023-11-14 00:28:50 +0000

    fix lint

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+const numP = parseInt(process.argv[2]);
+
+if (!numP) {
+  console.log('Missing size');
+} else {
+  let i = 0;
+  while (i < numP) {
+    console.log('X');
+    i++;
+  }
+}
