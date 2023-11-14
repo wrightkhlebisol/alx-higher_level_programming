758f480af94092f70b99396cbf89a8c1dbde45e1
wrightkhlebisol <calebogundiya@gmail.com>
2023-11-14 02:08:37 +0000

    second highest numbder

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,23 @@
 #!/usr/bin/node
+const pArgs = process.argv;
+
+if (!pArgs[2]) {
+  console.log(0);
+} else if (pArgs.length <= 3) {
+  console.log(0);
+} else {
+  let i = 2;
+  let firstHighest = 0;
+  let secondHighest = 0;
+
+  while (i < pArgs.length) {
+    if (pArgs[i] > firstHighest && pArgs[i] > secondHighest) {
+      secondHighest = firstHighest;
+      firstHighest = pArgs[i];
+    } else if (pArgs[i] > secondHighest && pArgs[i] < firstHighest) {
+      secondHighest = pArgs[i];
+    }
+    i++;
+  }
+  console.log(secondHighest);
+}
