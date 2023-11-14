ed8b7941b300eaf5424898657f7c64a6090a2685
wrightkhlebisol <calebogundiya@gmail.com>
2023-11-14 00:28:15 +0000

    factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+function factorial (num) {
+  if (isNaN(num)) {
+    return 1;
+  }
+
+  if (num === 0) {
+    return 1;
+  }
+
+  return (num * factorial(num - 1));
+}
+
+console.log(factorial(parseInt(process.argv[2])));
