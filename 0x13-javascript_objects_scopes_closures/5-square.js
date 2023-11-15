eccdb0e1633b1367d5958baaf839e09c60a6931b
wrightkhlebisol <calebogundiya@gmail.com>
2023-11-15 02:02:01 +0000

    square extends rectangle

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+const Rectangle = require('./4-rectangle.js');
+
+class Square extends Rectangle {
+
+  constructor (size) {
+    super(size, size);
+  }
+}
+
+module.exports = Square
