74a42113e2f0c6f32eeddf7511c7d173ab32579c
wrightkhlebisol <calebogundiya@gmail.com>
2023-11-15 02:12:06 +0000

    wprint conditionally

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,31 @@
 #!/usr/bin/node
+
+const Rectangle = require('./4-rectangle.js');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c) {
+      if (this.width && this.height) {
+        let i = 0;
+        let j = 0;
+        let rect = '';
+        while (i < this.height) {
+          while (j < this.width) {
+            rect += c;
+            j++;
+          }
+          console.log(rect);
+          i++;
+        }
+      }
+    } else {
+      this.print();
+    }
+  }
+}
+
+module.exports = Square;
