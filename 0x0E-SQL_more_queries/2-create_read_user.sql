995b0e8aea2f113e9308140f880024d136ac7578
wrightkhlebisol <calebogundiya@gmail.com>
2023-11-16 19:56:31 +0000

    create db, create user, grant

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,4 @@
-/** 
- * Creates the database hbtn_0d_2 and the user_0d_2
- * user_0d_2 password is user_0d_2_pwd
- */
+-- Creates the database hbtn_0d_2 and the user_0d_2
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
