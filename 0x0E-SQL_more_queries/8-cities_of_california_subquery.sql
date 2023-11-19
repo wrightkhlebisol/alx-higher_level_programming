a91a8d660f30f7e4cc65381fcb1f4cf6c65981c0
wrightkhlebisol <calebogundiya@gmail.com>
2023-11-19 08:47:37 +0000

    get cities of state

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,4 @@
 -- List all the cities of California in hbtn_0d_usa
-SELECT id, name FROM cities WHERE state_id = (SELECT id from states WHERE name='California');
+SELECT id, name FROM cities WHERE state_id = (
+    SELECT id FROM states WHERE name='California'  
+);
