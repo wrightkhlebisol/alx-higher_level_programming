5fcfa0998c52edd6205c8a6140a853ba9d209262
wrightkhlebisol <calebogundiya@gmail.com>
2023-11-19 22:01:23 +0000

    mysqldb select

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    host = 'localhost'
+    port = '3306'
+    url = f'{host}:{port}'
+    args = sys.argv
+    if len(args) >= 4:
+        db = MySQLdb.connect(host, args[1], args[2], args[3])
+        cur = db.cursor()
+        res = cur.execute("SELECT * FROM states")
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+        cur.close()
+        db.close()
