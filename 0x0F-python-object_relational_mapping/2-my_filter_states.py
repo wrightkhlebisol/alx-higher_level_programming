4c8aa7ca7bdfba2270d08f72c4286598aaf180f0
wrightkhlebisol <calebogundiya@gmail.com>
2023-11-20 19:06:41 +0000

    filter by state name

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+""" Filter states by user input """
+
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    host = 'localhost'
+    port = 3306
+    args = sys.argv
+    if len(args) >= 4:
+        db = MySQLdb.connect(
+            port=port,
+            host=host,
+            user=args[1],
+            password=args[2],
+            database=args[3]
+        )
+        print("Arg 4", args[4])
+        """ Create a cursor """
+        cur = db.cursor()
+        query = "SELECT * FROM states WHERE name = '{}' \
+            ORDER BY id ASC".format(args[4])
+        cur.execute(query)
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+        cur.close()
+        db.close()
