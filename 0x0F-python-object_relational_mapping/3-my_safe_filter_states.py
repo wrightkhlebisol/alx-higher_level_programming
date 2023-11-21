175777028419e78caaa1728909e2ec56d8e280d4
wrightkhlebisol <calebogundiya@gmail.com>
2023-11-21 14:19:50 +0000

    safe query

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+""" Filter states by user input safely """
+
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    host = 'localhost'
+    port = 3306
+    args = sys.argv
+    if len(args) >= 4:
+        db = MySQLdb.connect(
+            port=port,
+            host=host,
+            user=args[1],
+            password=args[2],
+            database=args[3]
+        )
+        """ Create a cursor """
+        cur = db.cursor()
+        query = "SELECT * FROM states WHERE name = %s \
+            ORDER BY id ASC"
+        cur.execute(query, (args[4], ))
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+        cur.close()
+        db.close()
