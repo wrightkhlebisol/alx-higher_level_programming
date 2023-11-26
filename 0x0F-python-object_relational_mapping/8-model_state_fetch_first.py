1d454cdb997d52e7d60091580dee86b91f0bcd36
wrightkhlebisol <calebogundiya@gmail.com>
2023-11-26 11:34:50 +0000

    get first res

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import State
+
+
+if __name__ == '__main__':
+    args = sys.argv
+
+    if len(args) >= 4:
+        user = args[1]
+        passwd = args[2]
+        db = args[3]
+        url = f'mysql+mysqldb://{user}:{passwd}@localhost:3306/{db}'
+
+        engine = create_engine(url)
+        session = Session(engine)
+
+        res = session.query(State).first()
+        print(f'{res.id}: {res.name}')
