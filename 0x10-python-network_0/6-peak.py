900b6930615ab70c19db837d8336a3e2a75c78e6
wrightkhlebisol <calebogundiya@gmail.com>
2023-12-04 16:43:57 +0000

    add shebang

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,9 +1,10 @@
+#!/usr/bin/python3
 """ Find peak in an integer """
 
 
 def find_peak(list_of_integers):
     """ Takes list of integers and find the highest """
-
+    """
     if len(list_of_integers) == 0:
         return None
 
@@ -20,4 +21,19 @@ def find_peak(list_of_integers):
         if p1 == p2:
             max_index = p2
 
-    return list_of_integers[max_index]
+    return list_of_integers[max_index] """
+    if not list_of_integers:
+        return None
+
+    low = 0
+    high = len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            high = mid
+        else:
+            low = mid + 1
+
+    return list_of_integers[low]
