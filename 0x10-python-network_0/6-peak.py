d47094ae334192afc7956acf6c82f12122a31125
wrightkhlebisol <calebogundiya@gmail.com>
2023-12-04 15:30:59 +0000

    find a peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,18 @@
+def find_peak(list_of_integers):
+    if len(list_of_integers) == 0:
+        return None
+
+    p1 = 0
+    p2 = len(list_of_integers) - 1
+    max_index = p2
+
+    while p1 != p2:
+        if list_of_integers[p1] > list_of_integers[p2]:
+            p2 -= 1
+        else:
+            p1 += 1
+
+        if p1 == p2:
+            max_index = p2
+
+    return list_of_integers[max_index]
