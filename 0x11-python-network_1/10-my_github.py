e8bf2fda9f506d0e11ea64d9035f72ab2a31e919
wrightkhlebisol <calebogundiya@gmail.com>
2023-12-06 20:11:36 +0000

    get user id

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,4 +1,24 @@
 #!/usr/bin/python3
+""" Takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id """
 
+if __name__ == '__main__':
+    import sys
+    import requests
+    username = sys.argv[1]
+    pa_token = sys.argv[2]
+    gith_url = f"https://api.github.com/users/{username}"
+    headers = {
+        'Authorization': pa_token,
+        "Accept": "application/vnd.github+json"
+    }
+    res = requests.get(gith_url, headers=headers)
+    try:
+        j_res = res.json()
 
-""""""
+        if j_res.get('id'):
+            print(j_res.get('id'))
+        else:
+            print(None)
+    except Exception as e:
+        print(e)
