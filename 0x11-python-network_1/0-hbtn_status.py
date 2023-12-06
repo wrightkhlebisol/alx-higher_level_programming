f5f02c2a5284897c43cc0c84b17485095cd57fb4
wrightkhlebisol <calebogundiya@gmail.com>
2023-12-06 03:27:30 +0000

    get type, content, msg

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Fetches status of url"""
+
+if __name__ == '__main__':
+    import urllib.request
+    with urllib.request.urlopen(
+        'https://alx-intranet.hbtn.io/status'
+    ) as response:
+        response_body = response.read()
+        print("Body response:")
+        print(f"\t - type: {type(response_body)}")
+        print(f"\t - content: {response_body}")
+        print(f"\t - utf8 content: {response.msg}")
