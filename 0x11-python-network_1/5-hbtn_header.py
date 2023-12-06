fc4457dacb8c0996eb85749bf71031f67d493694
wrightkhlebisol <calebogundiya@gmail.com>
2023-12-06 11:43:33 +0000

    get header if exist

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""Takes URL, sends a request to the URL and diplays the value of the variable X-Request-Id in the response header"""
+"""Takes URL, sends a request to the URL and diplays the
+value of the variable X-Request-Id in the response header"""
 import requests
 import sys
+
 if __name__ == '__main__':
     url = sys.argv[1]
     response = requests.get(url)
-    print(response.headers['X-Request-Id'])
+    if response.headers['X-Request-Id']:
+        print(response.headers['X-Request-Id'])
