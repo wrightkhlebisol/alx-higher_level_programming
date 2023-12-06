7c20de38ceb246eeafafa9f20648ee5940203242
wrightkhlebisol <calebogundiya@gmail.com>
2023-12-06 10:31:24 +0000

    request path and print

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
-
-
-""""""
+""" Use requests to query """
+if __name__ == '__main__':
+    import requests
+    url = "https://alx-intranet.hbtn.io/status"
+    response = requests.get(url)
+    print(f"""Body response:
+    - type: {type(response.text)} 
+    - content: {response.text} """)
