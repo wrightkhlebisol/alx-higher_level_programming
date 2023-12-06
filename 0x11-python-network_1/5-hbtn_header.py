2003f1eb4ec483a0bdaee1949ee12281b15b487b
wrightkhlebisol <calebogundiya@gmail.com>
2023-12-06 11:50:26 +0000

    get header, use getter

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -7,5 +7,4 @@ import sys
 if __name__ == '__main__':
     url = sys.argv[1]
     response = requests.get(url)
-    if response.headers['X-Request-Id']:
-        print(response.headers['X-Request-Id'])
+    print(response.headers.get('X-Request-Id'))
