5719518487002db091742090e9c4f88ec29cc89f
wrightkhlebisol <calebogundiya@gmail.com>
2023-12-06 09:28:50 +0000

    post body

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
-""" Send a POST request to the passed URL with email as param  """
-
+""" Send a POST request to the passed URL with email as param """
+if __name__ == '__main__':
+    import urllib.request
+    import sys
+    url = sys.argv[1]
+    body = {
+        'email': sys.argv[2]
+    }
+    with urllib.request.urlopen(url, body) as response:
+        print(response.read().decode('utf-8'))
