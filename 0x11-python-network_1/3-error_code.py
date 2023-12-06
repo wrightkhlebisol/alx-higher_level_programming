14c7b75a93ae0a5d3a2e5d5d72a7cddf24679c43
wrightkhlebisol <calebogundiya@gmail.com>
2023-12-06 10:13:10 +0000

    return response code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
-
-
-""""""
+""" Takes a URL, sends a request and displays response body in utf-8 """
+if __name__ == '__main__':
+    import sys
+    import urllib.request
+    path = sys.argv[1]
+    try:
+        with urllib.request.urlopen(path) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.getcode}")
