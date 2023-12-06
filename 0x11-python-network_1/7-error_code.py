5de091688bd75a958c9dda04216cd6893bee5817
wrightkhlebisol <calebogundiya@gmail.com>
2023-12-06 15:47:55 +0000

    print result or error code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
-
-
-""""""
+""" Takes a URL, sends a request to the URL
+displays the body of the response
+"""
+if __name__ == '__main__':
+    import requests
+    import sys
+    url = sys.argv[1]
+    try:
+        res = requests.get(url)
+        print(res.text)
+    except requests.exception.HTTPError as e:
+        if e >= 400:
+            print(f"Error code: {e.errno}")
