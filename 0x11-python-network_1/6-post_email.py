609e0fa5b118b68c88ec38a84f68f54a276d04c7
wrightkhlebisol <calebogundiya@gmail.com>
2023-12-06 12:12:06 +0000

    POST an email

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+""" Takes in a URL and an email address, sends a POST request
+to the passed URL with the email as a parameter, and
+finally displays the body of the response. """
+import requests
+import sys
 
-
-""""""
+if __name__ == '__name__':
+    url = sys.argv[1]
+    email = sys.argv[2]
+    res = requests.post(url, {'email': email})
+    print(res.text)
