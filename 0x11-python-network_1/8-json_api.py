d125680db1273349a56e86f9bd3dface017538ad
wrightkhlebisol <calebogundiya@gmail.com>
2023-12-06 16:08:12 +0000

    json or not

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
-
-
-""""""
+""" Takes in a letter and sends a POST request to
+    http://0.0.0.0:5000/search_user with the
+    letter as a parameter.
+"""
+if __name__ == '__main__':
+    import requests
+    import sys
+    url = 'http://0.0.0.0:5000/search_user'
+    q = sys.argv[1]
+    if not q:
+        q = ''
+    body = {'q': q}
+    res = requests.post(url, data=body)
+    if res:
+        print(f"[{res.id}] {res.name}")
+    else:
+        print("No result")
