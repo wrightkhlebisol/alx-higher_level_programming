323d9775a6e108d12e50ca346e65af45bbd98cef
wrightkhlebisol <calebogundiya@gmail.com>
2023-12-06 11:13:08 +0000

    extra space

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ Use requests to query """
+
 if __name__ == '__main__':
     import requests
     url = "https://alx-intranet.hbtn.io/status"
     response = requests.get(url)
     print(f"""Body response:
 - type: {type(response.text)}
-- content: {response.text}
-""")
+- content: {response.text}\n\n""")
