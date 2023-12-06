456d7962b244dbed54ff7c666023900557193dc6
wrightkhlebisol <calebogundiya@gmail.com>
2023-12-06 03:46:50 +0000

    get response header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+""" Fetch URL and return given header """
 
-
+if __name__ == '__main__':
+    import sys
+    import urllib.request
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        print(response.getheader('X-Request-Id'))
