206adec2a7f617acae166403b3383017be5ac094
wrightkhlebisol <calebogundiya@gmail.com>
2023-12-07 00:26:09 +0000

    get commits by user in repo

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python3
+"""
+List 10 commits (from the most recent to oldest) of
+the repository “rails” by the user “rails”
+"""
 
-
-""""""
+if __name__ == '__main__':
+    import sys
+    import requests
+    REPO = sys.argv[1]
+    OWNER = sys.argv[2]
+    gith_url = f"https://api.github.com/repos/{OWNER}/{REPO}/commits"
+    payload = {'per_page': '10', 'page': '1'}
+    res = requests.get(gith_url, params=payload)
+    try:
+        if len(res.json()) > 0:
+            for r in res.json():
+                print(f"{r.get('sha')}: {r.get('commit')['author']['name']}")
+        else:
+            print(None)
+    except Exception as e:
+        print(None, e)
