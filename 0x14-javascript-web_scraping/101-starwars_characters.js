70e9c28dbf9a3cb1e14c5e015cf9bdd61733661d
wrightkhlebisol <calebogundiya@gmail.com>
2024-01-04 23:28:22 +0000

    return response in order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -5,6 +5,28 @@ const process = require('process');
 const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
+function requestPromise(url){
+  return new Promise((resolve, reject) => {
+    request(url, (err, res, body) => {
+      if (err) {
+        return reject(err)
+      }
+      return resolve(body)
+    })
+  })
+}
+
+
+async function queryById(){
+  body = await requestPromise(url)
+  characterURL = JSON.parse(body).characters;
+  charResponse = characterURL.map(async url => await requestPromise(url))
+  characterDetails = await Promise.all(charResponse)
+  characterDetails.forEach((characterDetail) => console.log(JSON.parse(characterDetail).name))
+}
+
+queryById()
+/**
 request(url, (err, res, body) => {
   if (err) {
     return console.error(err);
@@ -14,7 +36,8 @@ request(url, (err, res, body) => {
       if (err) {
         return console.error(err);
       }
-      console.log(JSON.parse(body).name);
+      setTimeout(() => console.log(JSON.parse(body).name), 1000);
     });
   });
 });
+*/
