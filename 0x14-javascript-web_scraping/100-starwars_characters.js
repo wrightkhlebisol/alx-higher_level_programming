1839425c2f2d8f2c15e8fddf549f6e7be6bc265a
wrightkhlebisol <calebogundiya@gmail.com>
2024-01-04 02:33:00 +0000

    all chars of a star wars movie

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+const request = require('request');
+const process = require('process');
+
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+
+request(url, (err, res, body) => {
+  if (err) {
+    return console.error(err);
+  }
+  JSON.parse(body).characters.forEach(characterURL => {
+    request(characterURL, (err, res, body) => {
+      if (err) {
+        return console.error(err);
+      }
+      console.log(JSON.parse(body).name);
+    });
+  });
+});
