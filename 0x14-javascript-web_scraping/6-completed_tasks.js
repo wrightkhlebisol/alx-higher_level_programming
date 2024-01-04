f25dd908e2bb1ffe8cb2e40eae35c46d83972437
wrightkhlebisol <calebogundiya@gmail.com>
2024-01-04 02:14:56 +0000

    calculate completed todos

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+const request = require('request');
+const process = require('process');
+
+const url = process.argv[2];
+const totalCompleted = {};
+
+request(`${url}?completed=true`, (err, res, body) => {
+  if (err) {
+    return console.error(err);
+  }
+  const completedTasks = JSON.parse(body);
+  completedTasks.forEach(completedTask => {
+    totalCompleted[completedTask.userId]
+      ? totalCompleted[completedTask.userId]++
+      : totalCompleted[completedTask.userId] = 1;
+  });
+  console.log(totalCompleted);
+});
