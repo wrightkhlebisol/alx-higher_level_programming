53fcdeb59d989b4b9dd2661e36aea66e2b58cddb
wrightkhlebisol <calebogundiya@gmail.com>
2024-01-04 23:30:38 +0000

    fix standards

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -5,27 +5,26 @@ const process = require('process');
 const id = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 
-function requestPromise(url){
+function requestPromise (url) {
   return new Promise((resolve, reject) => {
     request(url, (err, res, body) => {
       if (err) {
-        return reject(err)
+        return reject(err);
       }
-      return resolve(body)
-    })
-  })
+      return resolve(body);
+    });
+  });
 }
 
-
-async function queryById(){
-  body = await requestPromise(url)
-  characterURL = JSON.parse(body).characters;
-  charResponse = characterURL.map(async url => await requestPromise(url))
-  characterDetails = await Promise.all(charResponse)
-  characterDetails.forEach((characterDetail) => console.log(JSON.parse(characterDetail).name))
+async function queryById () {
+  const body = await requestPromise(url);
+  const characterURL = JSON.parse(body).characters;
+  const charResponse = characterURL.map(async url => await requestPromise(url));
+  const characterDetails = await Promise.all(charResponse);
+  characterDetails.forEach((characterDetail) => console.log(JSON.parse(characterDetail).name));
 }
 
-queryById()
+queryById();
 /**
 request(url, (err, res, body) => {
   if (err) {
