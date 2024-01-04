aaab0b1c9fa521a9b85f44b5058674f94dc4feaa
wrightkhlebisol <calebogundiya@gmail.com>
2024-01-04 00:54:58 +0000

    starwars title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+const request = require('request');
+const process = require('process');
+
+const id = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
+
+request(url, (err, res, body) => {
+  if (err) {
+    console.error(err);
+  }
+  console.log(JSON.parse(body).title);
+});
