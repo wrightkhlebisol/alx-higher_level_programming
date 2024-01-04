fc0ae358903942fa88c334306dafa8ed9f098ddf
wrightkhlebisol <calebogundiya@gmail.com>
2024-01-04 01:47:07 +0000

    character count

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+const process = require('process');
+const request = require('request');
+
+const url = process.argv[2];
+const testStr = 'https://swapi-api.alx-tools.com/api/people/18/';
+let count = 0;
+
+request(url, (err, res, body) => {
+  if (err) {
+    return console.error(err);
+  }
+  JSON.parse(body).results.forEach(result => {
+    if (result.characters.includes(testStr)) {
+      count++;
+    }
+  });
+  console.log(count);
+});
