a4261f863a571cb64287359212b0d1bac923554c
wrightkhlebisol <calebogundiya@gmail.com>
2024-01-04 00:02:46 +0000

    write given content to given file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+const fs = require('fs');
+const process = require('process');
+
+const filePath = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(filePath, content, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+  }
+});
