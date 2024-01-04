5a7df52bdd6035dba9f13defc343f868387deddb
wrightkhlebisol <calebogundiya@gmail.com>
2024-01-04 00:14:26 +0000

    print statuscode

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const request = require('request');
+const process = require('process');
+
+const url = process.argv[2];
+request(url, (err, res, body) => {
+  if (err) {
+    console.error(err);
+  }
+  console.log(`code: ${res.statusCode}`);
+});
